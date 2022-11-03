645d366f2a4ac52b1312c6ed779eaa549becd10a
Widge-5 <77170359+Widge-5@users.noreply.github.com>
2022-11-03 21:58:08 +0000

    Update SBupdater.sh

--- home/pi/SBupdater.sh
+++ home/pi/SBupdater.sh
@@ -44,9 +44,6 @@ function do_everything () {
 
 
 function main () {
-  ## Create logfile ##
-  echo > SBupdate.log
-  
   ## Check if script has been run as root and, if not, quit ##
   if [[ $(id -u) -ne 0 ]] ; then echo "Please run as root" ; exit 1 ; fi
 
