b56544ad14afce215c6fe2020eede96c011388d7
Widge-5 <77170359+Widge-5@users.noreply.github.com>
2023-06-20 18:11:12 +0100

    Update Test Guns 1&2.sh

--- home/pi/RetroPie/roms/sinden/Test Guns 1&2.sh
+++ home/pi/RetroPie/roms/sinden/Test Guns 1&2.sh
@@ -4,13 +4,7 @@
   sudo rm /tmp/LightgunMono* -f
 } &> /dev/null
 
-
-DIR=/home/pi/Lightgun/Application
-utilscfg="/home/pi/Lightgun/utils/widgeutils.cfg"
-
-/home/pi/Lightgun/utils/testgrid.sh "$DIR"
-
-cd $DIR
+cd /home/pi/Lightgun/Application
 sudo mono LightgunMono.exe sdl 30
 
 # ON EXIT #
@@ -21,4 +15,3 @@ else
   sleep 3
 fi
 clear
-
