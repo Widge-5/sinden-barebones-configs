19289fe312e505c50f730a08831eef269e166493
Widge-5 <77170359+Widge-5@users.noreply.github.com>
2023-07-27 11:10:54 +0100

    Add files via upload

--- home/pi/RetroPie-Setup/scriptmodules/libretrocores/lr-opera-StormedBubbles.sh
+++ home/pi/RetroPie-Setup/scriptmodules/libretrocores/lr-opera-StormedBubbles.sh
@@ -13,7 +13,7 @@ rp_module_id="lr-opera-StormedBubbles"
 rp_module_desc="3DO emulator - fork of 4DO/FreeDO for libretro"
 rp_module_help="ROM Extension: .cue .chd .iso .zip\n\nCopy your 3do roms to $romdir/3do\n\nCopy the required BIOS file panazf10.bin to $biosdir"
 rp_module_licence="LGPL https://raw.githubusercontent.com/libretro/opera-libretro/master/libopera/opera_3do.c"
-rp_module_repo="git https://github.com/StormedBubbles/opera-libretro.git test-cursor-3DO"
+rp_module_repo="git https://github.com/StormedBubbles/opera-libretro.git hacks"
 rp_module_section="exp"
 
 function sources_lr-opera-StormedBubbles() {
